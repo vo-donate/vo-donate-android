9c5028ff7510fdfa2efd75b52b77cfd043dcd5f7
kevinlim17 <kevinlim17@icloud.com>
2025-05-18 23:42:11 +0900

    Add: Core:Network Module

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,39 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    alias(libs.plugins.jetpack.library)
+    alias(libs.plugins.jetpack.dagger.hilt)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.secrets)
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    namespace = "dev.kevin.core.network"
+}
+
+secrets {
+    defaultPropertiesFileName = "secrets.defaults.properties"
+}
+
+dependencies {
+    implementation(project(":core:android"))
+
+    // ... OkHTTP
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp.core)
+    implementation(libs.okhttp.logging)
+
+    // ... Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+
+    // ... Coil
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.svg)
+}
---
+++ core/network/src/main/kotlin/dev/kevin/core/network/JetpackRestApi.kt
@@ -0,0 +1,5 @@
+package dev.kevin.core.network
+
+class JetpackRestApi {
+
+}
